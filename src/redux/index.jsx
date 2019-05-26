20c682ca4317c055edbc2130ae9ff94944e08446
thundo811 <gvnnino811@gmail.com>
2019-05-26 16:52:55 +0700

    initial commit

--- src/redux/index.jsx
+++ src/redux/index.jsx
@@ -1,19 +1,13 @@
 //Store chính: Quản lý tất cả store con (Mỗi store con quản lý dữ liệu theo nghiệp vụ)
 import {combineReducers} from 'redux';
-import storeModalReducer from './reducers/Modal.reducer'
-import gioHangModalReducer from './reducers/gioHang.reducer'
-import quanLySanPhamReducer from './reducers/quanLySanPham.reducer'
-import sinhVienStoreReducer from './reducers/QuanLySinhVien.reducer'
+
 import gamePlayStorePreducer from './reducers/GamePlay.reducer'
 
 const rootReducer = combineReducers({
 
     //store con (store nghiệp vụ sau này)
     //storeModalReducer:storeModalReducer
-    storeModalReducer,
-    gioHangModalReducer,
-    quanLySanPhamReducer,
-    sinhVienStoreReducer,
+    
     gamePlayStorePreducer
 })
 
